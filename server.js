0a763cd23b90eef0e1cf056c8d4e3487b74b94c8
andrevan <andrevan.tm@gmail.com>
2022-01-23 21:44:56 +0900

    added message

--- server.js
+++ server.js
@@ -9,7 +9,7 @@ const HOST = '0.0.0.0';
 // App
 const app = express();
 app.get('/', (req, res) => {
-  res.send('Hello NodeJS in Jenkins World. Also hello from Docker');
+  res.send('Hello NodeJS in Jenkins World. Also hello from Docker. Hello CI/CD!');
 });
 
 app.listen(PORT, HOST);
